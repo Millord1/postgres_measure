43dcb8f415bf16216fa4de7603d977d05c7c8f1d
Millord1 <roffat.theau@outlook.com>
2026-06-17 16:58:40 +0200

    start pushing in database

--- database_push.py
+++ database_push.py
@@ -13,5 +13,11 @@ class DatabasePusher:
             """,item)
             self.db.conn.commit()
                
-    # def push_unique_transac(self, data: list[tuple]):
-        
+    def push_unique_transac(self, data: list[tuple]):
+        for item in data: 
+            self.db.cursor.execute(f"""
+                INSERT INTO {self.db.test_table} 
+                (number, texte, date, decimal) 
+                VALUES (%s,%s,%s,%s)
+            """,item)
+        self.db.conn.commit()
--- main.py
+++ main.py
@@ -8,28 +8,51 @@ PW = "Millord!"
 USER = "postgres"
 DB_NAME = "test"
 
+db = DataBase(DB_NAME, USER, PW)
+db_creator = DataBaseCreator(db)
+db_creator.create_test_table()
+
+db_pusher= DatabasePusher(db)
+seeder = Seeder()
+
+thousand_data = seeder.create_thousand()
+ht_data = seeder.create_hundred_thousand()
+million_data = seeder.create_million()
+
 def main():
+    push_naives()
+    push_batches()
+        
     
-    db = DataBase(DB_NAME, USER, PW)
-    db_creator = DataBaseCreator(db)
-    db_creator.create_test_table()
+def push_batches():
+    # TODO
+    pass
     
-    db_pusher= DatabasePusher(db)
-    seeder = Seeder()
-    thousand_data = seeder.create_thousand()
     
+def push_naives():
+    push_thousand_naive()
+    push_thousand_naive_transaction()
+
+    
+def push_thousand_naive():
     times = []
-    
     for _ in range(0,9):
         with Counter() as count:
             db_pusher.push_naive_version(thousand_data)   
         times.append(count.elapsed)
+    write_file("data/thousand_naive.txt", times)
     
-    filepath = Path("data/thousand_naive.txt")
+def push_thousand_naive_transaction():
+    times = []
+    for _ in range(0,9):
+        with Counter() as count:
+            db_pusher.push_naive_version(thousand_data)   
+        times.append(count.elapsed)
+    write_file("data/thousand_unique_tx.txt", times)
+
+def write_file(name: str, content):
+    filepath = Path(name)
     filepath.parent.mkdir(parents=True, exist_ok=True)
-    filepath.write_text("\n".join(map(str, times)), encoding="utf-8")
-        
-    print(times)
-    
+    filepath.write_text("\n".join(map(str, content)), encoding="utf-8")
     
 main()
